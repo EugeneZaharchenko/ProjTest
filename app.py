75408064bc8d5776d06c41931411b871b95a143b
eugene_zaharch <eugenezaharchenko@gmail.com>
2024-01-02 00:50:16 +0200

    products refactored

--- app.py
+++ app.py
@@ -10,54 +10,45 @@ users_list = [
 ]
 
 # База даних товарів
-products_list = [
-    {'id': 1, 'name': 'телефон', 'price': 800},
-    {'id': 2, 'name': 'рушниця', 'price': 6200},
-    {'id': 3, 'name': 'їжа', 'price': 100},
-    {'id': 3, 'name': 'набої', 'price': 50},
-    {'id': 3, 'name': 'стартовий пакет Київстар', 'price': 0},
-    {'id': 3, 'name': 'ноутбук', 'price': 40000}
-]
+categories = ['Всі', 'Електроніка', 'Їжа', 'Необхідне']
+
+products_list = {
+    'Електроніка': [{'name': 'телефон', 'price': 800},
+                    {'name': 'стартовий пакет Київстар', 'price': 0},
+                    {'name': 'ноутбук', 'price': 40000}
+                    ],
+    'Їжа': [{'name': 'шоколад', 'price': 100},
+            {'name': 'вода', 'price': 10},
+            {'name': 'сир', 'price': 150}
+            ],
+    'Необхідне': [{'name': 'рушниця', 'price': 6200},
+                  {'name': 'набої', 'price': 50}]
+
+}
 
 goods = []
 
 
-# Роут для додавання нових товарів
-@app.route("/add_product", methods=["POST"])
-def add_product():
-    product_data = request.get_json()
-    products_list.append(product_data)
-    return jsonify({"message": "Product added successfully"})
-
-
-# Роут для видавання товарів за категорією
-@app.route("/get_products_by_category", methods=["GET"])
-def get_products_by_category():
-    category = request.args.get("category")
-    category_products = [product for product in products_list if product["category"] == category]
-    return jsonify(category_products)
-
-
 # Роут для оформлення замовлення
-@app.route("/place_order", methods=["POST"])
-def place_order():
-    order_data = request.get_json()
-    user = order_data["user"]
-    selected_products = order_data["products"]
+# @app.route("/place_order", methods=["POST"])
+# def place_order():
+#     order_data = request.get_json()
+#     user = order_data["user"]
+#     selected_products = order_data["products"]
+#
+#     # Перевірка наявності товарів та коштів у користувача
+#     for product in selected_products:
+#         if product not in products:
+#             return jsonify({"message": f"Product {product} not available"})
+#     if user["balance"] < sum(product["price"] for product in selected_products):
+#         return jsonify({"message": "Insufficient funds"})
 
-    # Перевірка наявності товарів та коштів у користувача
-    for product in selected_products:
-        if product not in products:
-            return jsonify({"message": f"Product {product} not available"})
-    if user["balance"] < sum(product["price"] for product in selected_products):
-        return jsonify({"message": "Insufficient funds"})
-
-    # Оновлення балансу та видалення товарів зі списку наявних
-    user["balance"] -= sum(product["price"] for product in selected_products)
-    for product in selected_products:
-        products.remove(product)
-
-    return jsonify({"message": "Order placed successfully"})
+# Оновлення балансу та видалення товарів зі списку наявних
+# user["balance"] -= sum(product["price"] for product in selected_products)
+# for product in selected_products:
+#     products.remove(product)
+#
+# return jsonify({"message": "Order placed successfully"})
 
 
 @app.route("/main", methods=["GET"])
@@ -82,7 +73,35 @@ def add_user():
 
 @app.route("/products", methods=["GET"])
 def products():
-    return render_template("products.html", products=products_list)
+    _products = [product for products in products_list.values() for product in products]
+    return render_template("products.html", products=_products, categories=categories)
+
+
+# Роут для видавання товарів за категорією
+@app.route("/get_products_by_category", methods=["GET"])
+def get_products_by_category():
+    category = request.args.get("get_category") or 'Всі'
+    if category == 'Всі':
+        _products = [product for products in products_list.values() for product in products]
+        return render_template("products.html", products=_products, categories=categories)
+    else:
+        category_products = [product for product in products_list.get(category)]
+        return render_template('products.html', categories=categories, products=category_products)
+
+
+# Роут для додавання нових товарів
+@app.route('/add_product', methods=['GET', 'POST'])
+def add_product():
+    if request.method == 'POST':
+        category = request.form.get('category')
+        product_name = request.form.get('product_name')
+        product_price = request.form.get('product_price')
+
+        # Add the product to the specified category
+        products_list[category].append({'name': product_name, 'price': product_price})
+
+        return redirect(url_for('products'))
+    get_products_by_category()
 
 
 @app.route("/cart", methods=["GET"])
